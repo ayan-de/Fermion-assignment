91964898716ca8699cfbbf56427eeb6b94902ec9
Ayan De <deayan252@gmail.com>
2025-05-26 17:35:20 +0530

    feat: mediasoup server file setup with server root index.js file

--- apps/server/src/index.js
+++ apps/server/src/index.js
@@ -1,1 +1,29 @@
-// Entry point
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+const cors = require("cors");
+const mediasoup = require("mediasoup");
+
+const app = express();
+const server = http.createServer(app);
+
+//handling cors
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.use(cors());
+app.use(express.static("public")); // to serve HLS files
+
+const PORT = 4000;
+
+// Mediasoup logic
+const mediasoupServer = require("./mediasoupServer");
+//passing socket.io instance to the mediasoupServer
+mediasoupServer(io);
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
--- apps/server/src/mediasoupServer.js
+++ apps/server/src/mediasoupServer.js
@@ -1,1 +1,47 @@
 //Mediasoup router, transport, producer/consumer setup
+const mediasoup = require("mediasoup");
+
+//A Mediasoup worker is a background thread that handles all media traffic
+let worker;
+//Handles RTP capabilities and media routing between transports
+let router;
+// Object to track connected users (socket.id as key)
+let peers = {};
+
+module.exports = async function (io) {
+  worker = await mediasoup.createWorker();
+
+  //setting up which media codecs the server supports
+  //this handles forwarding streams between clients
+  router = await worker.createRouter({
+    mediaCodecs: [
+      {
+        kind: "audio",
+        mimeType: "audio/opus",
+        clockRate: 48000,
+        channels: 2,
+      },
+      {
+        kind: "video",
+        mimeType: "video/VP8",
+        clockRate: 90000,
+        parameters: {},
+      },
+    ],
+  });
+
+  //events for peer connection
+  io.on("connection", (socket) => {
+    console.log(`Client connected: ${socket.id}`);
+    peers[socket.id] = {};
+
+    //event for peer disconnect
+    socket.on("disconnect", () => {
+      console.log(`Client disconnected: ${socket.id}`);
+      delete peers[socket.id];
+    });
+
+    //TODO: Step-by-step: createTransport, connectTransport, produce, consume, etc.
+    // We’ll fill this out next
+  });
+};
